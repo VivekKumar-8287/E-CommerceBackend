55b39f5bd5d23551633badf56ee991c2edc36db9
Vivek Kumar <vivekgenius8287@gmail.com>
2025-02-03 19:19:53 +0530

    Coupon Route Complete

--- src/controllers/payment.ts
+++ src/controllers/payment.ts
@@ -15,3 +15,39 @@ export const newCoupon = TryCatch(async (req, res, next) => {
     message: `Coupon ${code} Created Successfully`,
   });
 });
+
+export const applyDiscount = TryCatch(async (req, res, next) => {
+  const { coupon } = req.query;
+
+  const discount = await Coupon.findOne({ code: coupon });
+
+  if (!discount) return next(new ErrorHandler("Invalid Coupon Code", 400));
+
+  return res.status(200).json({
+    success: true,
+    discount: discount.amount,
+  });
+});
+
+export const allCoupons = TryCatch(async (req, res, next) => {
+  const coupons = await Coupon.find({});
+
+  return res.status(200).json({
+    success: true,
+    coupons,
+  });
+});
+
+export const deleteCoupon = TryCatch(async (req, res, next) => {
+    const {id} = req.params;
+
+    const coupon = await Coupon.findByIdAndDelete(id);
+
+    if(!coupon) return next(new ErrorHandler("Invalid Coupon ID", 400))
+  
+    return res.status(200).json({
+      success: true,
+      message: `Coupon ${coupon.code} Deleted Successfully`,
+    });
+  });
+  
---
+++ src/controllers/stats.ts
@@ -0,0 +1,20 @@
+import { myCache } from "../app.js";
+import { TryCatch } from "../middlewares/error.js";
+
+export const getDashboardStats = TryCatch(async (req, res, next) => {
+  let stats = {};
+
+  if (myCache.has("admin-stats"))
+    stats = JSON.parse(myCache.get("admin-stats") as string);
+  else {
+  }
+
+  return res.status(200).json({
+    success: true,
+    stats,
+  });
+});
+
+export const getPieCharts = TryCatch(async () => {});
+export const getBarCharts = TryCatch(async () => {});
+export const getLineCharts = TryCatch(async () => {});
